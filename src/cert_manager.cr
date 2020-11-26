9477435dc8726455d71c51d9a8bcc54cb3c41ada
Juan Wajnerman <jwajnerman@manas.com.ar>
2020-11-26 15:26:46 -0300

    Use Process.run to execute openssl commands

--- src/cert_manager.cr
+++ src/cert_manager.cr
@@ -2,15 +2,15 @@ class Mitm::CertManager
   @ssl_contexts = Hash(String, OpenSSL::SSL::Context::Server).new
   @mutex = Mutex.new
   @certs_path : Path
-  @ca_crt_path : Path
-  @ca_key_path : Path
-  @ca_srl_path : Path
+  @ca_crt_path : String
+  @ca_key_path : String
+  @ca_srl_path : String
 
   def initialize(path)
     @root_path = Path.new(path)
-    @ca_crt_path = @root_path / "ca.crt"
-    @ca_key_path = @root_path / "ca.key"
-    @ca_srl_path = @root_path / "ca.srl"
+    @ca_crt_path = File.join(@root_path, "ca.crt")
+    @ca_key_path = File.join(@root_path, "ca.key")
+    @ca_srl_path = File.join(@root_path, "ca.srl")
     @certs_path = @root_path / "hosts"
 
     Dir.mkdir_p @certs_path
@@ -19,19 +19,20 @@ class Mitm::CertManager
   def context_for(host)
     @mutex.synchronize do
       @ssl_contexts[host] ||= begin
-        cert_file = @certs_path / "#{host}.crt"
-        key_file = @certs_path / "#{host}.key"
+        cert_file = File.join(@certs_path, "#{host}.crt")
+        key_file = File.join(@certs_path, "#{host}.key")
 
         unless File.exists?(cert_file) && File.exists?(key_file)
-          req_file = @certs_path / "#{host}.csr"
-          `openssl genrsa -out #{key_file} 2048`
-          `openssl req -new -sha256 -key #{key_file} -subj "/CN=#{host}" -out #{req_file}`
-          `openssl x509 -req -in #{req_file} -CA #{@ca_crt_path} -CAkey #{@ca_key_path} -CAcreateserial -CAserial #{@ca_srl_path} -out #{cert_file} -days 50000 -sha256`
+          req_file = File.join(@certs_path, "#{host}.csr")
+
+          Process.run("openssl", ["genrsa", "-out", key_file, "2048"])
+          Process.run("openssl", ["req", "-new", "-sha256", "-key", key_file, "-subj", "/CN=#{host}", "-out", req_file])
+          Process.run("openssl", ["x509", "-req", "-in", req_file, "-CA", @ca_crt_path, "-CAkey", @ca_key_path, "-CAcreateserial", "-CAserial", @ca_srl_path, "-out", cert_file, "-days", "50000", "-sha256"])
         end
 
         ssl_context = OpenSSL::SSL::Context::Server.new
-        ssl_context.certificate_chain = cert_file.to_s
-        ssl_context.private_key = key_file.to_s
+        ssl_context.certificate_chain = cert_file
+        ssl_context.private_key = key_file
         ssl_context
       end
     end
