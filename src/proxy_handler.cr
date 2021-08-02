7452c98c43a0ecbe1085ce2802c65cf4c332f69f
Anton K <83635660+anton7c3@users.noreply.github.com>
2021-08-02 11:25:03 +0300

    fix(websockets): added `websocket` to log messages (#3)
    Co-authored-by: Anton Karankevich <anton.karankevich@neuralegion.com>

--- src/proxy_handler.cr
+++ src/proxy_handler.cr
@@ -144,42 +144,42 @@ class Mitm::ProxyHandler
   # Connects two websockets to throw messages to each other
   private def match_websockets(client : ::HTTP::WebSocket, upstream : ::HTTP::WebSocket)
     client.on_message do |message|
-      Log.trace { "Client message #{message}" }
+      Log.trace { "Client websocket message #{message}" }
       upstream.send(message)
     end
 
     upstream.on_message do |message|
-      Log.trace { "Remote message #{message}" }
+      Log.trace { "Remote websocket message #{message}" }
       client.send(message)
     end
 
     client.on_binary do |message|
-      Log.trace { "Client binary message: #{message.hexstring}" }
+      Log.trace { "Client binary websocket message: #{message.hexstring}" }
       upstream.send(message)
     end
 
     upstream.on_binary do |message|
-      Log.trace { "Upstream binary message: #{message.hexstring}" }
+      Log.trace { "Upstream binary websocket message: #{message.hexstring}" }
       client.send(message)
     end
 
     client.on_ping do |message|
-      Log.trace { "Client ping #{message}" }
+      Log.trace { "Client websocket ping #{message}" }
       upstream.ping(message)
     end
 
     upstream.on_ping do |message|
-      Log.trace { "Remote ping #{message}" }
+      Log.trace { "Remote websocket ping #{message}" }
       client.ping(message)
     end
 
     client.on_pong do |message|
-      Log.trace { "Client pong #{message}" }
+      Log.trace { "Client websocket pong #{message}" }
       upstream.pong(message)
     end
 
     upstream.on_pong do |message|
-      Log.trace { "Remote pong #{message}" }
+      Log.trace { "Remote websocket pong #{message}" }
       client.pong(message)
     end
   end
