2e38942fb63f4910aea95cb34c886a1c878e96bd
Omer Ramić <ramic.omer82@gmail.com>
2021-10-27 15:50:24 +0200

    fix(cert): trim domain names larger than 64 chars (#7)
    * fix(cert): ignore insecure certifcates

--- src/cert_manager.cr
+++ src/cert_manager.cr
@@ -15,6 +15,11 @@ class Mitm::CertManager
   end
 
   def context_for(host)
+    # String for which we set certs should be less or equal to 64
+    if host.size > 64
+      domain_parts = host.split('.')
+      host = trim_long_domains(domain_parts) if domain_parts.size > 2 # First check if there is subodmain present in domain name
+    end
     @mutex.synchronize do
       @ssl_contexts[host] ||= begin
         cert_file = File.join(@certs_path, "#{host}.crt")
@@ -26,11 +31,24 @@ class Mitm::CertManager
           @generator.generate(host, cert_file, key_file)
         end
 
-        ssl_context = OpenSSL::SSL::Context::Server.new
+        ssl_context = OpenSSL::SSL::Context::Server.insecure
         ssl_context.certificate_chain = cert_file
         ssl_context.private_key = key_file
+        ssl_context.verify_mode = :none
+        ssl_context.ciphers = "ALL"
+        ssl_context.add_options(:all)
         ssl_context
       end
+    end
+  end
+
+  private def trim_long_domains(domain_parts : Array(String)) : String
+    domain_parts[0] = "*"           # Set subdomain to wildcard first, wildcard works with any subdomain name
+    domain = domain_parts.join(".") # Full name with subdomain format: *.something.example.com
+    if domain.size > 64 && domain_parts.size > 2
+      trim_long_domains(domain_parts[1..])
+    else
+      domain
     end
   end
 
